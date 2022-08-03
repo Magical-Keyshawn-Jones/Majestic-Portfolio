b664e070d4c9f17db1e7cb3e2e5a093640cb5f3d
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-08-02 22:22:36 -0500

    finished profile pic and technical super powers section

--- magical-portfolio/src/App.js
+++ magical-portfolio/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import profilePic from './Images/Keyshawn_Profile_Pic2.jpg'
 
 function App () {
   return (
@@ -9,6 +10,18 @@ function App () {
           <p>Full Stack Web Developer | Software Engineer</p>
         </div>
       </header>
+      <section>
+        <div>
+          <div className='profilePic'>
+            <img src={profilePic} alt='Profile_Picture'/>
+          </div>
+        </div>
+        <div className='technicalSkills'>
+          <h1>Technical Super Powers</h1>
+          <p className='lineSpace' ><span className='bold'>Frontend: </span>HTML, CSS, JavaScript, React.js, Redux, Hooks, Context API, Yup,  Jest, Cypress, Axios, Responsive Design</p>
+          <p><span className='bold' >Backend: </span>Node.js, Express, SQL, Git CLI, GitHub, VS Code, Vercel, Heroku, Knex</p>
+        </div>
+      </section>
     </main>
   )
 }
