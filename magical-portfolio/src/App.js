4e3462802c992761f61432cebf12f47ed387dbdc
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-08-02 20:45:49 -0500

    Starting over. I have liner gradient background and a header

--- magical-portfolio/src/App.js
+++ magical-portfolio/src/App.js
@@ -3,13 +3,10 @@ import './App.css';
 function App () {
   return (
     <main>
-      <div className = 'appHome'>
-        <div className='topSpace'></div>
-        <div className='middleSpace'> 
-          <p className='keyshawnJones'>Keyshawn Jones</p>
-        </div>
-        <div className='bottomSpace'></div>
-      </div>
+      <header>
+        <p>Keyshawn Jones</p>
+        <p>Full Stack Web Developer | Software Engineer</p>
+      </header>
     </main>
   )
 }
