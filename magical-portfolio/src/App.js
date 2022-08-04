ae209cfd6482f0af2514f8cc4de502b0b1bac9e7
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-08-04 14:17:52 -0500

    finished about me section

--- magical-portfolio/src/App.js
+++ magical-portfolio/src/App.js
@@ -22,6 +22,23 @@ function App () {
           <p><span className='bold' >Backend: </span>Node.js, Express, SQL, Git CLI, GitHub, VS Code, Vercel, Heroku, Knex</p>
         </div>
       </section>
+        <aside>
+          <h1>About Me</h1>
+          <p> I create the best code and solutions for any company as a full stack software engineer. Attention to detail, communication, and hard work are how I operate in any coding environment. Good vibes and positive energy are what you'll notice when I'm under pressure. Your company will make the best investment by hiring me.
+            <br/>
+            <br/>
+            ~My nickname is Doorknob
+            <br/>
+            ~I love the color pink
+            <br/>
+            ~I love to make people laugh
+            <br/>
+            <br/>
+            I love to code and work on my side projects. I play video games and hangout with friends in my spare time.
+            My future goal is to become a millionaire by the time I'm 30.
+            My dream to live a comfortable life with my family someday is a powerful dream that drives me.
+          </p>
+        </aside>
     </main>
   )
 }
